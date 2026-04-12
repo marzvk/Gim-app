921493bfb999d62d7998495fb03cdc68614f8a4b
marzvk <marianozovak1@gmail.com>
2026-04-11 22:49:30 -0300

    feat: Config de urls login , dashboard. Template dashboard.

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    """
+    Vista principal del dashboard
+    """
+    return render(request, "clientes/dashboard.html")
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # Login / Logout
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # Apps
+    path("", include("apps.clientes.urls")),
 ]
